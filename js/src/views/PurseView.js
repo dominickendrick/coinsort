9da099c9238a6a33d11fd2cfc99de0dfa65bad4b
Dominic Kendrick <dom@dominickendrick.co.uk>
2013-08-16 12:40:12 +0100

    Moving coin display table into view so it is less obtrusive

--- js/src/views/PurseView.js
+++ js/src/views/PurseView.js
@@ -1,5 +1,7 @@
 function PurseView(element, coins) {
 
+	var template = '<table id="coin_output"><thead><tr><th>Coin type</th><th>Quantity needed</th></tr></thead><tbody></tbody></table>';
+
 	var output = '';
 	
 	for(coin in coins){
@@ -8,5 +10,8 @@ function PurseView(element, coins) {
 		output += "</tr>";
 	}
 
+	if($('table#coin_output').length == 0){
+		$('section.main').append(template);
+	}
     $(element).html(output);
 }
