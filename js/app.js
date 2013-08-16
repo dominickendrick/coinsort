9337c5fbe96bccbc7fef1234d45003fa3ce69436
Dominic Kendrick <dom@dominickendrick.co.uk>
2013-08-16 15:53:29 +0100

    Using require.js to load scripts

--- js/app.js
+++ js/app.js
@@ -1,11 +1,28 @@
-$( document ).ready(function() {
-	
-	$('#coin_sort_submit').on('click', function(){
+requirejs.config({
+    //By default load any module IDs from js/lib
+    baseUrl: 'js',
 
-		purse = new PurseController();
-		purse.actionView($('#coin_sort_text').val());
-
-		return false;
-	});
-
+    paths: {
+        lib: 			'lib',
+		views: 			'src/views',
+		models: 		'src/models',
+		controllers:    'src/controllers',
+		utils: 			'src/utils'
+    }
 });
+
+// Start the main app logic.
+requirejs(['lib/jquery', 'utils/Utils', 'views/ErrorView', 'views/PurseView', 'controllers/PurseController', 'models/Purse'],
+function  () {
+
+	  $( document ).ready(function() {	
+		$('#coin_sort_submit').on('click', function(){	
+			purse = new PurseController();
+			purse.actionView($('#coin_sort_text').val());
+			return false;
+		});
+	
+	});
+});
+
+
