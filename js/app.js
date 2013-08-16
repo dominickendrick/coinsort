fd278f34bd0620771566b3992585e841f19dfdb0
Dominic Kendrick <dom@dominickendrick.co.uk>
2013-08-16 15:23:03 +0100

    Removing input checking from click event, the model/controller should be handling input validation

--- js/app.js
+++ js/app.js
@@ -1,11 +1,10 @@
 $( document ).ready(function() {
 	
 	$('#coin_sort_submit').on('click', function(){
-		if( $('#coin_sort_text').val()){
-			purse = new PurseController();
-			purse.actionView($('#coin_sort_text').val());
-			return false;
-		}
+
+		purse = new PurseController();
+		purse.actionView($('#coin_sort_text').val());
+
 		return false;
 	});
 
